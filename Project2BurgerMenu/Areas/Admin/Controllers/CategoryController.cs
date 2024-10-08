a12789fbe0f09f883560d66ecf88eea0b778e412
MuratYucedag <yucedagmurat23@gmail.com>
2024-10-08 15:43:18 +0300

    ...

---
+++ Project2BurgerMenu/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Project2BurgerMenu.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2BurgerMenu.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        BurgerMenuContext context = new BurgerMenuContext();
+        public ActionResult CategoryList()
+        {
+            var values = context.Categories.ToList();
+            return View(values);
+        }
+    }
+}
